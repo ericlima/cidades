83b4550197e1a8beb1ae357d09a831a7ca9643d5
eric <eric@L502x>
2018-02-13 12:55:26 -0200

    ajustes

--- cidades/src/main/java/br/edu/cse/controller/CidadeController.java
+++ cidades/src/main/java/br/edu/cse/controller/CidadeController.java
@@ -7,6 +7,7 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class CidadeController {
 	}
 	
 	@GetMapping("/cidade/{id}")
-	public Cidade obtemCidade(@PathParam(value = "id") Long id) {
+	public Cidade obtemCidade(@PathVariable Long id) {
 		return service.obtemCidade(id);
 	}
 	
